19cecd5126fb14cfb0c485bac8e5d2555578c92b
Oleksii Biletskyi <alexbily@gmail.com>
2022-07-29 13:14:18 +0300

    added local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,20 @@ class App extends Component {
     number: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(props, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = data => {
     this.addContact(data);
   };

32f549fdd59cb651dd30bbbb2f670eda4847e257
Oleksii Biletskyi <alexbily@gmail.com>
2022-08-09 15:45:41 +0300

    fixed input closing tag

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -5,12 +5,7 @@ import PropTypes from 'prop-types';
 const Filter = ({ value, onChange }) => (
   <label className={s.label}>
     Find contacts by name
-    <input
-      className={s.input}
-      type="text"
-      value={value}
-      onChange={onChange}
-    ></input>
+    <input className={s.input} type="text" value={value} onChange={onChange} />
   </label>
 );
 
